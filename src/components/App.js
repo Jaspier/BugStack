fc4829cc81c84905fc6109e9415ec81158979ed2
Jaspier <dungoallen349@gmail.com>
2021-10-09 16:47:40 +0100

    Creating the React UI for the 'bug stack'

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import LogItem from './LogItem';
 
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
-}
+  const [logs, setLogs] = useState([
+    {
+      _id: 1,
+      text: 'Log 1',
+      priority: 'low',
+      user: 'Allen',
+      created: new Date().toString(),
+    },
+    {
+      _id: 2,
+      text: 'Log 2',
+      priority: 'moderate',
+      user: 'Lance',
+      created: new Date().toString(),
+    },
+    {
+      _id: 3,
+      text: 'Log 3',
+      priority: 'high',
+      user: 'Daru',
+      created: new Date().toString(),
+    },
+  ]);
 
-export default App
+  return (
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Priority</th>
+            <th>Log Text</th>
+            <th>User</th>
+            <th>Created</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map(log => (
+            <LogItem key={log._id} log={log} />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default App;
