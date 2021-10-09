e359d4cb1ba3c097172db94bed03d00542b19f80
Jaspier <dungoallen349@gmail.com>
2021-10-09 17:30:44 +0100

    Ability to add a bug log (frontend only)

--- src/components/App.js
+++ src/components/App.js
@@ -2,41 +2,49 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import LogItem from './LogItem';
+import AddLogItem from './AddLogItem';
 
 const App = () => {
   const [logs, setLogs] = useState([
     {
       _id: 1,
-      text: 'Log 1',
+      text: 'Bug 1',
       priority: 'low',
       user: 'Allen',
       created: new Date().toString(),
     },
     {
       _id: 2,
-      text: 'Log 2',
+      text: 'Bug 2',
       priority: 'moderate',
       user: 'Lance',
       created: new Date().toString(),
     },
     {
       _id: 3,
-      text: 'Log 3',
+      text: 'Bug 3',
       priority: 'high',
       user: 'Daru',
       created: new Date().toString(),
     },
   ]);
 
+  function addItem(item) {
+    item._id = Math.floor(Math.random() * 9000) + 10000;
+    item.created = new Date().toString();
+    setLogs([...logs, item]);
+  }
+
   return (
     <Container>
+      <AddLogItem addItem={addItem} />
       <Table>
         <thead>
           <tr>
             <th>Priority</th>
-            <th>Log Text</th>
+            <th>Bug</th>
             <th>User</th>
-            <th>Created</th>
+            <th>Logged</th>
             <th></th>
           </tr>
         </thead>
